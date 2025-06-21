73214850a85ae7ab1ead266c1eb5e1e8ed0156b1
Yuta <jp.yuta0131@gmail.com>
2025-06-21 21:00:40 +0900

    update

--- lua/yoru/init.lua
+++ lua/yoru/init.lua
@@ -28,6 +28,10 @@ function M.setup()
     -- Main Text
     Normal = { fg = colors.text, bg = colors.background },
     NormalFloat = { link = "Normal" },
+    Underlined = { fg = colors.green },
+    Ignore = { fg = colors.blue },
+    Error = { link = "DiagnosticError" },
+    Todo = { link = "DiagnosticInfo" },
 
     -- Syntax Highlighting
     Comment = { fg = colors.white },
@@ -35,7 +39,7 @@ function M.setup()
     Character = { fg = colors.green },
     Number = { fg = colors.cyan },
     Boolean = { fg = colors.cyan },
-    Identifier = { fg = colors.text },
+    Identifier = { fg = colors.blue },
     Function = { fg = colors.purple },
     Keyword = { fg = colors.magenta },
     Type = { fg = colors.yellow },
@@ -44,11 +48,74 @@ function M.setup()
     Delimiter = { fg = colors.white },
     Debug = { fg = colors.yellow },
 
+    -- Diff
+    Added = { fg = colors.green },
+    Changed = { fg = colors.blue },
+    Removed = { fg = colors.magenta },
+
+    -- Completion / Popups
+    Pmenu = { fg = colors.text, bg = colors.selection },
+    PmenuSel = { fg = colors.black, bg = colors.blue },
+    NotifyBackground = { bg = colors.selection },
+
+    -- Diagnostics
+    DiagnosticError = { fg = colors.red },
+    DiagnosticWarn = { fg = colors.yellow },
+    DiagnosticInfo = { fg = colors.blue },
+    DiagnosticHint = { fg = colors.magenta },
+    DiagnosticOk = { fg = colors.green },
+
+    -- Treesitter
+    ["@variable"] = { link = "Identifier" },
+    ["@constant"] = { link = "Constant" },
+    ["@module"] = { fg = colors.yellow },
+    ["@string"] = { link = "String" },
+    ["@string.special.url"] = { link = "String" },
+    ["@character"] = { link = "Character" },
+    ["@boolean"] = { link = "Boolean" },
+    ["@number"] = { link = "Number" },
+    ["@number.float"] = { link = "Float" },
+    ["@type"] = { link = "Type" },
+    ["@type.builtin"] = { link = "Keyword" },
+    ["@property"] = { fg = colors.magenta },
+    ["@function"] = { link = "Function" },
+    ["@function.builtin"] = { link = "Function" },
+    ["@keyword"] = { link = "Keyword" },
+    ["@punctuation.delimiter"] = { link = "Delimiter" },
+    ["@punctuation.bracket"] = { link = "Bracket" },
+    ["@punctuation.special"] = { link = "Special" },
+    ["@comment"] = { link = "Comment" },
+    ["@comment.error"] = { link = "DiagnosticError" },
+    ["@comment.warning"] = { link = "DiagnosticWarn" },
+    ["@comment.todo"] = { link = "DiagnosticInfo" },
+    ["@comment.note"] = { link = "DiagnosticHint" },
+    ["@markup.heading"] = { fg = colors.blue, bold = true },
+    ["@markup.link.url"] = { link = "@string.special.url" },
+    ["@diff.plus"] = { link = "Added" },
+    ["@diff.minus"] = { link = "Removed" },
+    ["@diff.delta"] = { link = "Changed" },
+    ["@tag"] = { link = "Tag" },
+    ["@tag.attribute"] = { link = "@property" },
+
+    -- LSP
+    ["@lsp.type.namespace"] = { link = "Module" },
+
     -- Oil.nvim
-    OilHidden = { fg = colors.gray },
+    OilHidden = { fg = colors.white },
     OilDir = { link = "Directory" },
     OilDirHidden = { link = "OilHidden" },
     OilFile = { fg = colors.white },
+
+    -- Mini Icons
+    MiniIconsAzure = { fg = colors.blue },
+    MiniIconsBlue = { fg = colors.blue },
+    MiniIconsCyan = { fg = colors.cyan },
+    MiniIconsGreen = { fg = colors.green },
+    MiniIconsGrey = { fg = colors.white },
+    MiniIconsOrange = { fg = colors.orange },
+    MiniIconsPurple = { fg = colors.purple },
+    MiniIconsRed = { fg = colors.red },
+    MiniIconsYellow = { fg = colors.yellow },
 
     -- Statusline (Simple.nvim)
     StatusLineNormalA = { fg = colors.background, bg = colors.blue, bold = true },
