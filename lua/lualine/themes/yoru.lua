4acff8875d98adbc2b8dcc75be6f3b9fe3db5092
Yuta <jp.yuta0131@gmail.com>
2025-08-18 18:15:19 +0900

    update

--- lua/lualine/themes/yoru.lua
+++ lua/lualine/themes/yoru.lua
@@ -6,31 +6,26 @@ return {
     b = { bg = colors.background2, fg = colors.blue },
     c = { bg = colors.background1, fg = colors.text },
   },
-
   insert = {
     a = { bg = colors.green, fg = colors.background, gui = "bold" },
     b = { bg = colors.background2, fg = colors.green },
     c = { bg = colors.background1, fg = colors.text },
   },
-
   terminal = {
     a = { bg = colors.yellow, fg = colors.background, gui = "bold" },
     b = { bg = colors.background2, fg = colors.yellow },
     c = { bg = colors.background1, fg = colors.text },
   },
-
   command = {
     a = { bg = colors.orange, fg = colors.background, gui = "bold" },
     b = { bg = colors.background2, fg = colors.orange },
     c = { bg = colors.background1, fg = colors.text },
   },
-
   visual = {
     a = { bg = colors.purple, fg = colors.background, gui = "bold" },
     b = { bg = colors.background2, fg = colors.purple },
     c = { bg = colors.background1, fg = colors.text },
   },
-
   replace = {
     a = { bg = colors.red, fg = colors.background, gui = "bold" },
     b = { bg = colors.background2, fg = colors.red },
