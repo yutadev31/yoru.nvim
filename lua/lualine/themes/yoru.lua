fc3f37f6d39ffcdfdec55f1a62e6631233737e95
Yuta <jp.yuta0131@gmail.com>
2025-09-10 03:46:34 +0900

    update

--- lua/lualine/themes/yoru.lua
+++ lua/lualine/themes/yoru.lua
@@ -3,37 +3,37 @@ local colors = require("yoru.colors")
 return {
   normal = {
     a = { bg = colors.blue, fg = colors.background, gui = "bold" },
-    b = { bg = colors.background2, fg = colors.blue },
-    c = { bg = colors.background1, fg = colors.text },
+    b = { bg = colors.statusbar, fg = colors.blue },
+    c = { bg = "NONE", fg = colors.text },
   },
   insert = {
     a = { bg = colors.green, fg = colors.background, gui = "bold" },
-    b = { bg = colors.background2, fg = colors.green },
-    c = { bg = colors.background1, fg = colors.text },
+    b = { bg = colors.statusbar, fg = colors.green },
+    c = { bg = "NONE", fg = colors.text },
   },
   terminal = {
     a = { bg = colors.yellow, fg = colors.background, gui = "bold" },
-    b = { bg = colors.background2, fg = colors.yellow },
-    c = { bg = colors.background1, fg = colors.text },
+    b = { bg = colors.statusbar, fg = colors.yellow },
+    c = { bg = "NONE", fg = colors.text },
   },
   command = {
     a = { bg = colors.orange, fg = colors.background, gui = "bold" },
-    b = { bg = colors.background2, fg = colors.orange },
-    c = { bg = colors.background1, fg = colors.text },
+    b = { bg = colors.statusbar, fg = colors.orange },
+    c = { bg = "NONE", fg = colors.text },
   },
   visual = {
     a = { bg = colors.purple, fg = colors.background, gui = "bold" },
-    b = { bg = colors.background2, fg = colors.purple },
-    c = { bg = colors.background1, fg = colors.text },
+    b = { bg = colors.statusbar, fg = colors.purple },
+    c = { bg = "NONE", fg = colors.text },
   },
   replace = {
     a = { bg = colors.red, fg = colors.background, gui = "bold" },
-    b = { bg = colors.background2, fg = colors.red },
-    c = { bg = colors.background1, fg = colors.text },
+    b = { bg = colors.statusbar, fg = colors.red },
+    c = { bg = "NONE", fg = colors.text },
   },
   inactive = {
     a = { bg = colors.text1, fg = colors.background, gui = "bold" },
-    b = { bg = colors.background2, fg = colors.text1 },
-    c = { bg = colors.background1, fg = colors.text },
+    b = { bg = colors.statusbar, fg = colors.text1 },
+    c = { bg = "NONE", fg = colors.text },
   },
 }
