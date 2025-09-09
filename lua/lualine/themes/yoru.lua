4430dc098cb02d52fc5d1e8fed9ddf861269b987
Yuta <jp.yuta0131@gmail.com>
2025-09-10 04:06:44 +0900

    update

--- lua/lualine/themes/yoru.lua
+++ lua/lualine/themes/yoru.lua
@@ -3,37 +3,37 @@ local colors = require("yoru.colors")
 return {
   normal = {
     a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.statusbar, fg = colors.blue },
+    b = { bg = colors.overlay, fg = colors.blue },
     c = { bg = colors.bg, fg = colors.fg },
   },
   insert = {
     a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.statusbar, fg = colors.green },
+    b = { bg = colors.overlay, fg = colors.green },
     c = { bg = colors.bg, fg = colors.fg },
   },
   terminal = {
     a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.statusbar, fg = colors.yellow },
+    b = { bg = colors.overlay, fg = colors.yellow },
     c = { bg = colors.bg, fg = colors.fg },
   },
   command = {
     a = { bg = colors.orange, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.statusbar, fg = colors.orange },
+    b = { bg = colors.overlay, fg = colors.orange },
     c = { bg = colors.bg, fg = colors.fg },
   },
   visual = {
     a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.statusbar, fg = colors.purple },
+    b = { bg = colors.overlay, fg = colors.purple },
     c = { bg = colors.bg, fg = colors.fg },
   },
   replace = {
     a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.statusbar, fg = colors.red },
+    b = { bg = colors.overlay, fg = colors.red },
     c = { bg = colors.bg, fg = colors.fg },
   },
   inactive = {
     a = { bg = colors.white, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.statusbar, fg = colors.white },
+    b = { bg = colors.overlay, fg = colors.white },
     c = { bg = colors.bg, fg = colors.fg },
   },
 }
